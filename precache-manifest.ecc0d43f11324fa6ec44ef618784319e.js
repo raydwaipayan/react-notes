91fbfa95bec4ed9e502869c9c4ae96b50380e184
raydwaipayan <dwaipayanray1@gmail.com>
2019-05-12 19:37:28 +0530

    Updates

---
+++ precache-manifest.ecc0d43f11324fa6ec44ef618784319e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d597591f858230aa384121073804544",
+    "url": "/react-notes/index.html"
+  },
+  {
+    "revision": "4ad1bd2dd41c6e1b0d3b",
+    "url": "/react-notes/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "851a2facf9813d0b2b22",
+    "url": "/react-notes/static/js/2.7650623f.chunk.js"
+  },
+  {
+    "revision": "4ad1bd2dd41c6e1b0d3b",
+    "url": "/react-notes/static/js/main.27fdcc3f.chunk.js"
+  },
+  {
+    "revision": "f88c5e79517469ec5f27",
+    "url": "/react-notes/static/js/runtime~main.22bb4f5c.js"
+  }
+]);
